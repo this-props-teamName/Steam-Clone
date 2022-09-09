9bbceeff97831cbb33b0fc5e656c36e25522afa6
RRFernandez <98431164+RRFernandez@users.noreply.github.com>
2022-09-09 16:37:53 -0400

    Added lang dropdown with links and CSS header file for styling.

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
+import LangStyle from '../styles/header.module.css'
 
 
 const Header = () => {
   const [onHoverStore, setOnHoverStore] = useState(false); 
-  const [onHoverComm, setOnHoverComm] = useState(false); 
+  const [onHoverComm, setOnHoverComm] = useState(false);
+  const [onLangMenu, setOnLangMenu] = useState(false);
 
 
   function storeClass() {
@@ -11,6 +13,9 @@ const Header = () => {
   }
  function commClass() {
   return `overflow-none ${onHoverComm  ? ' absolute z-[1500] opacity-1 left-[59px] top-[64px] bg-[#171a21] max-w-[132px] p-[10px]  text-left' : 'hidden'}`
+  }
+  function langMenu() {
+    return `${onLangMenu ? 'block top-[25px] -left-[72.3203px] opacity-100 align-top z-200 absolute shadow-[0_0_12px_#000000]' : 'hidden'}`
   }
 
   
@@ -140,9 +145,41 @@ const Header = () => {
               login
             </a>
             &nbsp; |&nbsp;
-            <button className=" inline-block pl-[4px] leading-[25px] mr-[5px] pr-[18px] bg-[url('https://store.akamai.steamstatic.com/public/shared/images/popups/btn_arrow_down_padded.png')] bg-no-repeat bg-[center_right] text-[12px] hover:text-[#FFFFFF]">
+            <button className=" inline-block pl-[4px] leading-[25px] mr-[5px] pr-[18px] bg-[url('https://store.akamai.steamstatic.com/public/shared/images/popups/btn_arrow_down_padded.png')] bg-no-repeat bg-[center_right] text-[12px] hover:text-[#FFFFFF]" onClick={() => setOnLangMenu(!onLangMenu)}>
               language
             </button>
+            <div className={langMenu()}>
+              <div className='pt-1.5 pr-0 pb-1.5 pl-0 border-[#171a21] border-solid border-[0px] relative bg-[#171a21]'>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/_/?l=schinese'>简体中文 (Simplified Chinese)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/_/?l=tchinese'>繁體中文 (Traditional Chinese)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/_/?l=japanese'>日本語 (Japanese)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/_/?l=koreana'>한국어 (Korean)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/Hogwarts_Legacy/?l=thai'>ไทย (Thai)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/Hogwarts_Legacy/?l=bulgarian'>Български (Bulgarian)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/Hogwarts_Legacy/?l=czech'>Čeština (Czech)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/Hogwarts_Legacy/?l=danish'>Dansk (Danish)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/Hogwarts_Legacy/?l=german'>Deutsch (German)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/Hogwarts_Legacy/?l=spanish'>Español - España (Spanish - Spain)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/Hogwarts_Legacy/?l=latam'>Español - Latinoamérica (Spanish - Latin America)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/Hogwarts_Legacy/?l=greek'>Ελληνικά (Greek)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/Hogwarts_Legacy_LHritage_de_Poudlard/?l=french'>Français (French)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/Hogwarts_Legacy/?l=italian'>Italiano (Italian)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/Hogwarts_Legacy/?l=hungarian'>Magyar (Hungarian)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/Hogwarts_Legacy/?l=dutch'>Nederlands (Dutch)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/Hogwarts_Legacy/?l=norwegian'>Norsk (Norwegian)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/Dziedzictwo_Hogwartu/?l=polish'>Polski (Polish)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/Hogwarts_Legacy/?l=portuguese'>Português (Portuguese - Portugal)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/Hogwarts_Legacy/?l=brazilian'>Português - Brasil (Portuguese - Brazil)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/Hogwarts_Legacy/?l=romanian'>Română (Romanian)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/_/?l=russian'>Русский (Russian)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/Hogwarts_Legacy/?l=finnish'>Suomi (Finnish)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/Hogwarts_Legacy/?l=swedish'>Svenska (Swedish)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/Hogwarts_Legacy/?l=turkish'>Türkçe (Turkish)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/Hogwarts_Legacy/?l=vietnamese'>Tiếng Việt (Vietnamese)</a>
+                <a className={LangStyle.langWindow} href='https://store.steampowered.com/app/990080/Hogwarts_Legacy/?l=ukrainian'>Українська (Ukrainian)</a>
+                <a className={LangStyle.langWindow} href='https://www.valvesoftware.com/en/contact?contact-person=Translation%20Team%20Feedback'>Report a translation problem</a>
+              </div>
+            </div>
             {/* --------this is the log in version ----------------- */}
             <div className="hidden inline-block relative leading-[24px] mr-[3px]">
               <a className=" hidden inline-block pl-[35px] pr-[9px] bg-[bottom_5px_left_10px] bg-[#616a72] bg-[url('https://store.akamai.steamstatic.com/public/shared/images/header/btn_header_installsteam_download.png?v=1')] bg-no-repeat leading-[24px] text-[11px] h-[24px] text-[#e5e4dc] hover:text-[#FFFFFF]">
