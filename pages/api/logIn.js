29afac783061040ca09a88cc775d60bceafd5ece
Alexander Cobble <acobble@Alexanders-MacBook-Pro.local>
2022-09-08 19:29:14 -0700

    added req.js, logIn.js, and min.js to api folder

---
+++ pages/api/logIn.js
@@ -0,0 +1,11 @@
+import pool from '../../backend/server'
+
+export default function handler(req, res, next) {
+    if(req.method === 'GET'){
+        pool.query('SELECT * FROM logIn').then((data) =>{
+            res.send(data.rows);
+        })
+        .catch(next);
+    }
+  }
+  
