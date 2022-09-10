ba00ededa2f8b67ad3a40f62b833bb100ed766bd
Alexander Cobble <acobble@Alexanders-MacBook-Pro.local>
2022-09-10 13:25:28 -0700

    added the header and footer component imports

---
+++ pages/login.js
@@ -0,0 +1,14 @@
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+const login = () => {
+  return (
+    <>
+      <Header />
+      <div>login</div>
+      <Footer />
+    </>
+  )
+}
+
+export default login
