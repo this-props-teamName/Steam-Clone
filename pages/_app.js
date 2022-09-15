1a4235a85b3f7be45d90de5e75415d85ed52fde8
Alexander Cobble <acobble@Alexanders-MacBook-Pro.local>
2022-09-15 15:39:16 -0700

    Auth0 installed trial

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,18 @@ import Layout from '../components/Layout'
 import {RecoilRoot} from 'recoil';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import { UserProvider } from '@auth0/nextjs-auth0'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <RecoilRoot>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </RecoilRoot>
+      <UserProvider>
+        <RecoilRoot>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </RecoilRoot>
+      </UserProvider>
     </>
   )
 }
